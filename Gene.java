18ee5d9a7abda07d95ebd93507d25d110b603282
Grant Lemons <grant0lemons@gmail.com>
2021-10-24 21:14:12 -0500

    remove the check for over all string divisibility by 3 because a classmate told me thats not true

--- Gene.java
+++ Gene.java
@@ -9,39 +9,36 @@ public class Gene
 
     public void getORF()
     {
-        if (sequence.length() % 3 == 0)
+        int pos = 0;
+        while (sequence.length()-1 > pos)
         {
-            int pos = 0;
-            while (sequence.length()-1 > pos)
+            int startIndex = sequence.indexOf("ATG", pos);
+
+            if (startIndex >= 0)
             {
-                int startIndex = sequence.indexOf("ATG", pos);
+                int finalIndex0 = sequence.indexOf("TAA", startIndex);
+                int finalIndex1 = sequence.indexOf("TAG", startIndex);
+                int finalIndex2 = sequence.indexOf("TGA", startIndex);
+                Integer myEnd = null;
 
-                if (startIndex >= 0)
+                myEnd = setEnd(startIndex, finalIndex0, myEnd);
+                myEnd = setEnd(startIndex, finalIndex1, myEnd);
+                myEnd = setEnd(startIndex, finalIndex2, myEnd);
+
+                if (myEnd != null)
                 {
-                    int finalIndex0 = sequence.indexOf("TAA", startIndex);
-                    int finalIndex1 = sequence.indexOf("TAG", startIndex);
-                    int finalIndex2 = sequence.indexOf("TGA", startIndex);
-                    Integer myEnd = null;
-
-                    myEnd = setEnd(startIndex, finalIndex0, myEnd);
-                    myEnd = setEnd(startIndex, finalIndex1, myEnd);
-                    myEnd = setEnd(startIndex, finalIndex2, myEnd);
-
-                    if (myEnd != null)
-                    {
-                        String subsequence = sequence.substring(startIndex, myEnd+3);
-                        System.out.println(subsequence);
-                        pos = myEnd;
-                    }
-                    else
-                    {
-                        pos = sequence.length();
-                    }
+                    String subsequence = sequence.substring(startIndex, myEnd+3);
+                    System.out.println(subsequence);
+                    pos = myEnd;
                 }
                 else
                 {
                     pos = sequence.length();
                 }
+            }
+            else
+            {
+                pos = sequence.length();
             }
         }
     }

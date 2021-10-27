7f587bda02e7d6d277165908c041aabde01e1a81
Grant Lemons <grantlemons@users.noreply.github.com>
2021-10-27 12:53:45 -0500

    change to test method

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args)
     {
-        Gene gene = new Gene("0000000000000ATG000000000000000000000000000000TGA0000TAG000TAA00000ATG000000TGA00000ATG000000TAA000");
-        gene.getORF();
+        Gene gene = new Gene();
+        gene.test();
     }
 }

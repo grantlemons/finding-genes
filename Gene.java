a23b599bfb762db66bbe1d4728f961ea8648f8cc
Grant Lemons <grantlemons@users.noreply.github.com>
2021-10-27 12:53:06 -0500

    add test method

--- Gene.java
+++ Gene.java
@@ -1,14 +1,33 @@
 public class Gene
-{
-    private String sequence;
-
-    public Gene(String senseStrand)
+{    
+    public void test()
     {
-        sequence = senseStrand;
+        String[] testCases = new String[12];
+        testCases[0] = "ATGxxxTAG"; // Normal w/ TAG
+        testCases[1] = "ATGxxxTAA"; // Normal w/ TAA
+        testCases[2] = "ATGxxxTGA"; // Normal w/ TGA
+        testCases[3] = "xxxATGxxxTGA"; // Leading 'x's
+        testCases[4] = "xxxATGxxxTGAxxTAA"; // Extraneous End Codon
+        testCases[5] = "TAG"; // Only End Codon
+        testCases[6] = "ATG"; // Only Start Codon
+        testCases[7] = "xx"; // No Codon
+        testCases[8] = ""; // Empty string
+        testCases[9] = "ATGxxTAG"; // Incorrect Spacing
+        testCases[10] = "ATGxxxTAG   ATGxxxTAA"; // Two seperate genomes
+        testCases[11] = "ATGxxxTAG ATGxxxTAA"; // Two seperate genomes w/ spacing not multiple of three
+        
+        Gene gene = new Gene();
+        
+        for (int i=0; i < testCases.length; i++)
+        {
+            System.out.println("Test Case "+ i + " ----------- \""+ testCases[i]+"\"");
+            gene.getORF(testCases[i]);
+        }
     }
 
-    public void getORF()
+    public void getORF(String sequence)
     {
+        boolean foundORF = false;
         int pos = 0;
         while (sequence.length()-1 > pos)
         {
@@ -20,16 +39,16 @@ public class Gene
                 int finalIndex1 = sequence.indexOf("TAG", startIndex);
                 int finalIndex2 = sequence.indexOf("TGA", startIndex);
                 Integer myEnd = null;
-                int end = sequence.length();
-
-                myEnd = setEnd(startIndex, finalIndex0, myEnd);
-                myEnd = setEnd(startIndex, finalIndex1, myEnd);
-                myEnd = setEnd(startIndex, finalIndex2, myEnd);
+                
+                myEnd = setEnd(startIndex, finalIndex0, myEnd, sequence);
+                myEnd = setEnd(startIndex, finalIndex1, myEnd, sequence);
+                myEnd = setEnd(startIndex, finalIndex2, myEnd, sequence);
 
                 if (myEnd != null)
                 {
                     String subsequence = sequence.substring(startIndex, myEnd+3);
-                    System.out.println(subsequence);
+                    System.out.println(startIndex+".."+myEnd+": "+subsequence);
+                    foundORF = true;
                     pos = myEnd;
                 }
                 else
@@ -42,9 +61,13 @@ public class Gene
                 pos = sequence.length();
             }
         }
+        if (!foundORF)
+        {
+            System.out.println("No genomes found");
+        }
     }
 
-    private Integer setEnd(int sIndex, int fIndex, Integer myEnd)
+    private Integer setEnd(int sIndex, int fIndex, Integer myEnd, String sequence)
     {
         if (myEnd == null)
         {
@@ -61,10 +84,5 @@ public class Gene
             }
         }
         return myEnd;
-    }
-
-    public String toString()
-    {
-        return sequence;
     }
 }

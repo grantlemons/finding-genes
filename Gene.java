9ad1ad552cafedb5c8226557d3159187c76e9694
Grant Lemons <grantlemons@users.noreply.github.com>
2021-11-05 14:13:08 -0500

    add comments

--- Gene.java
+++ Gene.java
@@ -1,65 +1,72 @@
 import edu.duke.*;
-import org.apache.commons.logging.*;
 
 public class Gene
 {
-    private Log log = LogFactory.getLog(Gene.class);
-
+    // Test cases to determine if the algorithm does as required
     public void test()
     {
-        getORF("ATGxxxTAG"); // Normal w/ TAG
-        getORF("ATGxxxTAA"); // Normal w/ TAA
-        getORF("ATGxxxTGA"); // Normal w/ TGA
-        getORF("xxxATGxxxTGA"); // Leading 'x's
-        getORF("xxxATGxxxTGAxxTAA"); // Extraneous End Codon
-        getORF("TAG"); // Only End Codon
-        getORF("ATG"); // Only Start Codon
-        getORF("xx"); // No Codon
-        getORF(""); // Empty string
-        getORF("ATGxxTAG"); // Incorrect Spacing
-        getORF("ATGxxxTAG   ATGxxxTAA"); // Two seperate genomes
-        getORF("ATGxxxTAG ATGxxxTAA"); // Two seperate genomes w/ spacing not multiple of three
-        getORF("xxATGxx"); // end - stop % 3 == 0
+        getGenome("ATGxxxTAG"); //Normal w/ TAG
+        getGenome("ATGxxxTAA"); //Normal w/ TAA
+        getGenome("ATGxxxTGA"); //Normal w/ TGA
+        getGenome("xxxATGxxxTGA"); //Leading 'x's
+        getGenome("xxxATGxxxTGAxxTAA"); //Extraneous End Codon
+        getGenome("TAG"); //Only End Codon
+        getGenome("ATG"); //Only Start Codon
+        getGenome("xx"); //No Codon
+        getGenome(""); //Empty string
+        getGenome("ATGxxTAG"); //Incorrect Spacing
+        getGenome("ATGxxxTAG   ATGxxxTAA"); //Two seperate genomes
+        getGenome("ATGxxxTAG ATGxxxTAA"); //Two seperate genomes w/ spacing not multiple of three
+        getGenome("xxATGxx"); //end - stop % 3 == 0
+        getGenome("ATGxxxTAATGxxx"); //Combined start and stop codons
     }
     
+    // Loads the sense strand file, parses it, and changes to upper case
     public String loadFile()
     {
         FileResource file = new FileResource("sensestrand.txt");
+        
+        // Use regex to remove whitespace
+        // Change to upper case
         return file.asString().replaceAll("[^a-zA-Z]", "").toUpperCase();
     }
 
-    public void getORF(String sequence)
+    // Method to find the start and stop codons, and print everything in between
+    public void getGenome(String sequence)
     {
         boolean foundGenome = false;
         int pos = 0;
+        
         while (sequence.length()-1 > pos)
         {
             int startIndex = sequence.indexOf("ATG", pos);
 
             if (startIndex >= 0)
             {
-                Integer myEnd = null;
-                
-                myEnd = setEnd(startIndex, "TAA", myEnd, sequence);
-                myEnd = setEnd(startIndex, "TAG", myEnd, sequence);
-                myEnd = setEnd(startIndex, "TGA", myEnd, sequence);
+                // Finds the first valid stop codon after the start codon
+                int myEnd = 0;
+                myEnd = setEnd(startIndex, "TAA", sequence, myEnd);
+                myEnd = setEnd(startIndex, "TAG", sequence, myEnd);
+                myEnd = setEnd(startIndex, "TGA", sequence, myEnd);
 
-                if (myEnd != null)
+                if (myEnd != 0)
                 {
                     String subsequence = sequence.substring(startIndex, myEnd+3);
+                    
                     System.out.println(startIndex+1+".."+(myEnd+2+1)+": "+subsequence);
-                    //log.info(startIndex+1+".."+(myEnd+2+1)+": "+subsequence);
+                    printAntiSense(startIndex, myEnd, subsequence, sequence.length());
+
                     foundGenome = true;
-                    pos = myEnd;
+                    pos = myEnd+3;
                 }
                 else
                 {
-                    pos = sequence.length();
+                    return;
                 }
             }
             else
             {
-                pos = sequence.length();
+                return;
             }
         }
         if (!foundGenome)
@@ -68,30 +75,63 @@ public class Gene
         }
     }
 
-    private Integer setEnd(int sIndex, String searchString, Integer myEnd, String sequence)
+    // Finds the end codon by looping through all possible end codons until a valid one if found
+    private int setEnd(int sIndex, String searchString, String sequence, int myEnd)
     {
         int fIndex = sIndex;
-        while (fIndex != -1 && fIndex < sequence.length()-2)
+        
+        // Loops through all possible end codons of the given type until one is found
+        while (fIndex != -1 && fIndex < sequence.length())
         {
             fIndex = sequence.indexOf(searchString, fIndex+1);
-            if ((fIndex-sIndex) % 3 == 0 && fIndex > -1)
+            
+            // Checks to make sure the end codon is valid
+            if (((fIndex-sIndex) % 3 == 0 && fIndex != -1) && (myEnd == 0 || fIndex < myEnd))
             {
-                if (myEnd == null)
-                {
-                    return Integer.valueOf(fIndex);
-                }
-                else if (fIndex < myEnd.intValue())
-                {
-                    return Integer.valueOf(fIndex);
-                }
+                return fIndex;
             }
         }
         return myEnd;
     }
+
+    // Method to change a sense strand into antisense strand and print it
+    private void printAntiSense(int sIndex, int fIndex, String subsequence, int length)
+    {
+        String newString = "";
+        
+        // Build antisense strand
+        for (int i = 0; i < subsequence.length(); i++)
+        {
+            char cur = subsequence.charAt(i);
+
+            if (cur == 'A')
+            {
+                newString = 'T' + newString;
+            }
+            else if (cur == 'T')
+            {
+                newString = 'A' + newString;
+            }
+
+            else if (cur == 'C')
+            {
+                newString = 'G' + newString;
+            }
+            else if (cur == 'G')
+            {
+                newString = 'C' + newString;
+            }
+        }
+        
+        int AntiSIndex = (length - fIndex);
+        int AntiFIndex = (length - sIndex);
+        System.out.println(AntiSIndex+".."+AntiFIndex+": "+newString);
+    }
     
+    // Main runnable file to find sense and antisense strands from a file
     public static void main(String[] args)
     {
         Gene gene = new Gene();
-        gene.getORF(gene.loadFile());
+        gene.getGenome(gene.loadFile());
     }
 }
